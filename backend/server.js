10d4d5b680a795c18de596d0e0aaa1707c290f7f
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-17 09:07:55 +0100

    Added headers to allow CORS

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,19 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE,PATCH, OPTIONS'
+  );
+  next();
+});
+
 app.use('/api/users', userRouter);
 
 app.listen(3000, () => {
